bf165661a48bfd1a9a19fb36dee45e15f5a62080
Danya <116107083+EscaLag@users.noreply.github.com>
2023-01-16 01:01:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import pyzipper
-
 import os
+import threading
+from time import sleep
 from datetime import datetime
 
-import tkinter as tk
+from pyzipper import AESZipFile
 from tkinter import filedialog
-import threading
-import time
 
 tries = 0
 
 os.system("title ZiPCracker")
+
 #Helpers
 def cls():
     os.system("cls")
@@ -18,31 +17,27 @@ def cls():
 def title(total):
     while True:
         os.system(f"title {tries}/{total}")
-        time.sleep(0.00001)
+
 #Main
 def crack(file, wordlist):
     global tries
     cls()
     print("Loading Wordlist")
-    words = []
-    
     os.system("title Loading wordlist")
-    wordlistsize = 0
+    
     startTime = datetime.now()
-    with open (wordlist.name, "r", encoding="latin-1") as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.replace("\n", "")
-            wordlistsize += 1
-            words.append(line)
-    print(f"Word list loaded. Took: {(datetime.now() - startTime).total_seconds()}")
+    words = open(wordlist.name, 'r').read()
+    words = words.split('\n')
+    wordlistsize = len(words)
+
+    print(f"Word list loaded. Took: {(datetime.now() - startTime).total_seconds()}ms")
     foldername = file.name.split("/")[-1]
     foldername = foldername.split(".")[0]
-    x = threading.Thread(target=title, args=(str(wordlistsize),))
-    x.start()
+    threading.Thread(target=title, args=(str(wordlistsize),)).start()
+    print('password selection started')
     for word in words:
         try: 
-            with pyzipper.AESZipFile(file.name, "r") as z:
+            with AESZipFile(file.name, "r") as z:
                 z.extractall(foldername, pwd=bytes(word, 'utf-8'))
         except PermissionError:
             print("I don't have permission to create a folder here!")
@@ -54,17 +49,18 @@ def crack(file, wordlist):
             os.system(f"explorer \"{os.getcwd()}\\{foldername}\"")
             return
     print("The password was not found. Try using another wordlist")
-root = tk.Tk()
-root.withdraw()
-print("What file do you want to crack")
-file = filedialog.askopenfile(mode='r', title="Select a file to crack", initialdir=os.getcwdb(), filetypes=(("Zip file", ".zip"),))
-print(file)
-print("Select your wordlist (If you don't have search for \"Rockyou\")")
-wordlist = filedialog.askopenfile(mode='r', title="Wordlist", initialdir=os.getcwdb(), filetypes=(("Text files", ".txt"),))
-try:
-    crack(file, wordlist)
-    print("Program stopped")
-    os.system("pause >nul")
-except KeyboardInterrupt:
-    print("leaving")
-    time.sleep(2)
+
+
+if __name__ == '__main__':
+    print("What file do you want to crack")
+    file = filedialog.askopenfile(mode='r', title="Select a file to crack", initialdir=os.getcwdb(), filetypes=(("Zip file", ".zip"),))
+    print(file)
+    print("Select your wordlist (If you don't have search for \"Rockyou\")")
+    wordlist = filedialog.askopenfile(mode='r', title="Wordlist", initialdir=os.getcwdb(), filetypes=(("Text files", ".txt"),))
+    try:
+        crack(file, wordlist)
+        print("Program stopped")
+        os.system("pause >nul")
+    except KeyboardInterrupt:
+        print("leaving")
+        sleep(2)
